324c602e75384da7cdee7b50bd6eb5b4ad56896d
Thorsten Krug <tho@donkeymedia.eu>
2020-12-10 12:05:58 +0100

    Add dependency colors and improve UX.

--- css_color_variables_ui/source/js/css-color-variables-ui.js
+++ css_color_variables_ui/source/js/css-color-variables-ui.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 import AWN from 'awesome-notifications';
 import color from 'color';
 
-(async ($, drupalSettings) => {
+(async ($, drupalSettings, document) => {
   /**
    * @type {Drupal~behavior}
    */
@@ -40,7 +40,6 @@ import color from 'color';
               toggleButton.removeClass('disabled').addClass('enabled');
               this.activateColorChanger();
             } else {
-              toggleButton.removeClass('enabled').addClass('disabled');
               this.deactivateColorChanger();
             }
           }
@@ -54,19 +53,39 @@ import color from 'color';
       $('body').append(drupalSettings.css_color_variables_ui.html_template);
       const self = this;
       const settings = drupalSettings.css_color_variables_ui;
-
       if (!drupalSettings.css_color_variables_ui.colorInfo) {
         // Load fields and schemas.
         drupalSettings.css_color_variables_ui.colorInfo = await this.getColorInfo();
       }
+      // Collect CSS vars so we can update derivated colors.
+      settings.rootCssVariables = this.getRootCSS();
 
       // eslint-disable-next-line prefer-destructuring
       const colors = settings.colorInfo.schemes._current.colors;
       Object.keys(colors).forEach(key => {
         const value = colors[key];
         const button = $(`#color-schema-ui [data-color="${key}"]`);
+        let previousValue = value;
         let picker = null;
-        self.updateButtonTextColor(button, value);
+        let derivates = null;
+        // Check for derivate colors.
+        if (key.indexOf("__") === -1) {
+          derivates = this.getDerivates(key);
+        }
+        self.setButtonTextColor(button, value);
+        const setValue = (hexVal) => {
+          // Change CSS variable.
+          document.documentElement.style.setProperty(
+            self.toCssVar(key),
+            hexVal
+          );
+          // Change button text color.
+          self.setButtonTextColor(button, hexVal);
+          // Update derivate CSS variables.
+          if (derivates) {
+            this.updateDerivateCss(hexVal, derivates);
+          }
+        };
         button.on({
           click: () => {
             if (!picker) {
@@ -74,14 +93,20 @@ import color from 'color';
                 parent: button[0],
                 alpha: false,
                 color: value,
+                cancelButton: true,
+                onClose: () => {
+                  setValue(previousValue);
+                },
+                onDone: newVal => {
+                  // Converting HEX RGBA to color_module 6-digit hex.
+                  const hexVal = newVal.hex.substr(0, 7);
+                  previousValue = hexVal;
+                  setValue(hexVal);
+                },
                 onChange: self.debounce(newVal => {
                   // Converting HEX RGBA to color_module 6-digit hex.
                   const hexVal = newVal.hex.substr(0, 7);
-                  document.documentElement.style.setProperty(
-                    self.toCssVar(key),
-                    hexVal
-                  );
-                  self.updateButtonTextColor(button, newVal.hex);
+                  setValue(hexVal);
                 }, 5)
               });
               picker.setColor(this.getCurrentCssColorByName(key));
@@ -103,13 +128,19 @@ import color from 'color';
       $('#color-schema-ui button.save').on({
         click: this.save.bind(this)
       });
+      $('#color-schema-ui button.cancel').on({
+        click: this.deactivateColorChanger.bind(this)
+      });
     },
 
     /**
      * Close UI.
      */
     deactivateColorChanger() {
+      $('.color-schema-ui-toggle').removeClass('enabled').addClass('disabled');
       $('#color-schema-ui').remove();
+      // Currently required we need new variants
+      window.location.reload();
     },
 
     /**
@@ -183,7 +214,7 @@ import color from 'color';
      * @return string
      *   Forground color hex string.
      */
-    updateButtonTextColor(button, value) {
+    setButtonTextColor(button, value) {
       button.css('color', color(value).luminosity() > 0.5 ? '#000' : '#fff');
     },
 
@@ -223,6 +254,117 @@ import color from 'color';
         text += chars.charAt(Math.floor(Math.random() * chars.length));
       }
       return text;
+    },
+
+    /**
+     * Get all CSS root variables.
+     *
+     * With ♥ from tnt-rox
+     * @see https://stackoverflow.com/a/54851636/308533
+     * @return Array
+     */
+    getRootCSS() {
+      return Array.from(document.styleSheets)
+        .filter(
+          sheet =>
+            sheet.href === null || sheet.href.startsWith(window.location.origin)
+        )
+        .reduce(
+          (acc, sheet) =>
+            (acc = [
+              ...acc,
+              ...Array.from(sheet.cssRules).reduce(
+                (def, rule) =>
+                  (def =
+                    rule.selectorText === ":root"
+                      ? [
+                        ...def,
+                        ...Array.from(rule.style).filter(name =>
+                          name.startsWith("--")
+                        )
+                      ]
+                      : def),
+                []
+              )
+            ]),
+          []
+        );
+      ;
+    },
+
+    /**
+     * Find all derivates to a color.
+     *
+     * @param color
+     *  Color basename
+     */
+    getDerivates(color) {
+      const derivates = [];
+      const derivateCssVariables = drupalSettings.css_color_variables_ui.rootCssVariables.forEach((cssVar) => {
+        if (cssVar.startsWith(`--${color}--`)) {
+          const params = cssVar.substr(`--${color}--`.length).split('-');
+          derivates.push({
+            name:  color,
+            action: params[0],
+            value: params[1],
+            cssvar: cssVar,
+          })
+        }
+      });
+      return derivates;
+    },
+
+    /**
+     * Find all derivates to a color.
+     *
+     * @param color
+     *  Color basename
+     */
+    async updateDerivateCss(color, derivates) {
+      derivates.forEach((d) => {
+        switch(d.action) {
+          case 'darken':
+            document.documentElement.style.setProperty(d.cssvar, this.adjustColorLuminance(color, -1 * d.value/100 ));
+            break;
+          case 'lighten':
+            document.documentElement.style.setProperty(d.cssvar, this.adjustColorLuminance(color, d.value/100 ));
+            break;
+          case 'opacity':
+            const opaHex = Math.round((256 * d.value / 100)).toString(16);
+            document.documentElement.style.setProperty(d.cssvar, `${color}${opaHex}`);
+            break;
+          case 'contrast':
+            // Do nothing.
+            break;
+          default:
+            console.warn(`Unknown action '${d.action}' at updateDerivateCss : '${d.cssvar}'`)
+        }
+      })
+    },
+
+    /**
+     * Adjust color brightness.
+     *
+     * @param hex
+     * @param lum
+     * @return {string}
+     */
+    adjustColorLuminance(hex, lum) {
+      // validate hex string
+      hex = String(hex).replace(/[^0-9a-f]/gi, '');
+      if (hex.length < 6) {
+        hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+      }
+      lum = lum || 0;
+      // convert to decimal and change luminosity
+      var rgb = "#", c, i;
+      for (i = 0; i < 3; i++) {
+        c = parseInt(hex.substr(i*2,2), 16);
+        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+        rgb += ("00"+c).substr(c.length);
+      }
+      return rgb;
     }
-  };
-})(window.jQuery, window.drupalSettings);
+
+  }
+})(window.jQuery, window.drupalSettings, window.document);
